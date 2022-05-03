61ed9c1a32c5f14693b563dc5f0598e8572f5f79
Duro <davidwright13503@gmail.com>
2022-05-03 19:58:54 -0400

    Update logger

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,3 +1,6 @@
+import {
+  blue, cyan, magenta, red, yellow,
+} from 'chalk';
 import { LogLevel, SapphireClient } from '@sapphire/framework';
 import { Intents } from 'discord.js';
 import { env } from './env';
@@ -12,7 +15,33 @@ export class UserClient extends SapphireClient {
       ],
       loadDefaultErrorListeners: true,
       logger: {
-        level: LogLevel.Debug,
+        level: LogLevel.Info,
+        format: {
+          debug: {
+            timestamp: null,
+            infix: magenta.bold('\b\b[DEBUG] '),
+          },
+          info: {
+            timestamp: null,
+            infix: cyan.bold('\b\b[INFO] '),
+          },
+          warn: {
+            timestamp: null,
+            infix: yellow.bold('\b\b[WARN] '),
+          },
+          error: {
+            timestamp: null,
+            infix: red.bold('\b\b[ERROR] '),
+          },
+          fatal: {
+            timestamp: null,
+            infix: red.bold('\b\b[FATAL] '),
+          },
+          trace: {
+            timestamp: null,
+            infix: blue.bold(`${'\b'.repeat(7)}[TRACE] `),
+          },
+        },
       },
     });
   }
